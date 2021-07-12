8579365c46164c14762d6aa6a0e1a9584f7d7215
Ross Greenall <rossgreenall@gmail.com>
2021-07-12 18:08:17 +0100

    Set up simple note tracker with toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    const [notesList, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState('a new note...');
+    const [showAll, setShowAll] = useState(true);
+
+    const addNote = (e) => {
+        e.preventDefault();
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+            id: notesList.length + 1,
+        };
+
+        setNotes(notesList.concat(noteObject));
+        setNewNote('');
+    };
+
+    const handleNoteChange = (e) => {
+        console.log(e.target.value);
+        setNewNote(e.target.value);
+    };
+
+    const notesToShow = showAll
+        ? notesList
+        : notesList.filter((note) => note.important === true);
+
+    return (
+        <div className="App">
+            <h1>Notes</h1>
+
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">Save Note</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
